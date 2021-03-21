001ab6dbad8eee7945c6793f8fec3eb7293fdaa4
Yug34 <53560276+Yug34@users.noreply.github.com>
2021-03-22 02:42:34 +0530

    Added comments

--- src/index.js
+++ src/index.js
@@ -2,16 +2,22 @@ const buttons = document.getElementsByClassName("keypadBtn");
 const output = document.getElementById("output");
 
 for (const button of buttons) {
+  // index counter to keep track of clicks on a button
   let index = 0;
+  // time in ms for hold and busy timeouts
   const delay = 500;
-  let isBusy = true;
   let hold = null;
   let busy = null;
+  let isBusy = true;
+  // variable to store last click
   let click = null;
 
   button.onmousedown = function (e) {
     const dataText = this.getAttribute("data-text").split("");
     const dataNumber = this.getAttribute("data-number");
+    
+    // if the click is on subText class element
+    // save the click's target as it's parent element.
     const target = e.target.classList.contains("subText")
       ? e.currentTarget
       : e.target;
@@ -20,10 +26,12 @@ for (const button of buttons) {
 
     clearTimeout(busy);
 
+    // If current click != prev click, change isBusy status
     if (click !== target) {
       isBusy = false;
     }
 
+    // Loop the index variable around
     if (index >= dataText.length - 1 || click !== target) {
       index = 0;
       click = target;
@@ -31,12 +39,14 @@ for (const button of buttons) {
       index = index + 1;
     }
 
+    // if mouse is held down, remove last char and add number in place
     hold = setTimeout(() => {
       if (dataNumber) {
         output.value = output.value.slice(0, -1) + dataNumber;
       }
     }, delay);
 
+    // only remove the last character if isBusy is true
     output.value = isBusy
       ? output.value.slice(0, -1) + dataText[index]
       : output.value + dataText[index];
