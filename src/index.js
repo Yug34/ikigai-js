626d742cb6d7d442b477345a2a9a3fccc7d77493
Yug34 <vertilax1337@gmail.com>
2021-03-21 17:46:49 +0530

    Completed implementing logic, linting, testing

---
+++ src/index.js
@@ -0,0 +1,55 @@
+const buttons = document.querySelectorAll("button");
+const output = document.getElementById("output");
+
+for (const button of buttons) {
+  let index = 0;
+  const delay = 500;
+  let isBusy = true;
+  let hold = null;
+  let busy = null;
+  let click = null;
+
+  button.onmousedown = function (e) {
+    const dataText = this.getAttribute("data-text").split("");
+    const dataNumber = this.getAttribute("data-number");
+    const target = e.target.classList.contains("subText")
+      ? e.currentTarget
+      : e.target;
+
+    isBusy = true;
+
+    clearTimeout(busy);
+
+    if (click !== target) {
+      isBusy = false;
+    }
+
+    if (index >= dataText.length - 1 || click !== target) {
+      index = 0;
+      click = target;
+    } else {
+      index = index + 1;
+    }
+
+    hold = setTimeout(() => {
+      if (dataNumber) {
+        output.value = output.value.slice(0, -1) + dataNumber;
+      }
+    }, delay);
+
+    output.value = isBusy
+      ? output.value.slice(0, -1) + dataText[index]
+      : output.value + dataText[index];
+  };
+
+  button.onmouseup = () => {
+    clearTimeout(hold);
+    isBusy = true;
+
+    busy = setTimeout(() => {
+      index = -1;
+      isBusy = false;
+      click = null;
+    }, delay);
+  };
+}
